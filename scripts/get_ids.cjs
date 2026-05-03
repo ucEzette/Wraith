d1beca212097883471ffa46aae7a3e3e087ec767
ucEzette <u.ezette@alustudent.com>
2026-05-03 12:25:17 +0200

    feat(frontend): implement liquidity management routes: force_liquidity.cjs, get_ids.cjs

--- scripts/get_ids.cjs
+++ scripts/get_ids.cjs
@@ -1,10 +1,11 @@
 const { ethers } = require("ethers");
 
-const HOOK = "0x7Da6934Dc1231398C63DE28051588775B1b70280".toLowerCase();
-const USDC = "0x06Afd270830607994D5a12248443B1f531393A22".toLowerCase();
+const HOOK = "0x62B207729023CD2544a5BE6791f1bb77fb2cc280".toLowerCase();
+const USDC = "0x31d0220469e10c4E71834a79b1f276d740d3768F".toLowerCase();
 const WETH = "0x4200000000000000000000000000000000000006".toLowerCase();
 const QPHAN = "0x9d803A3066C858d714C4F5eE286eaa6249d451aB".toLowerCase();
 const ECHO = "0x6586035D5e39e30bf37445451b43EEaEeAa1405a".toLowerCase();
+const WRAITH = "0x9dA26648257a17bEB42d9464663b7b9Ce1c4f174".toLowerCase();
 const ETH = "0x0000000000000000000000000000000000000000".toLowerCase();
 
 const FEE = 3000;
@@ -24,7 +25,8 @@ function getPoolId(tokenA, tokenB) {
     return ethers.keccak256(encoded);
 }
 
-console.log("1. ETH / USDC:", getPoolId(ETH, USDC));
-console.log("2. WETH / USDC:", getPoolId(WETH, USDC));
-console.log("3. QPHAN / USDC:", getPoolId(QPHAN, USDC));
-console.log("4. ECHO / USDC:", getPoolId(ECHO, USDC));
+console.log("1. WRAITH / USDC:", getPoolId(WRAITH, USDC));
+console.log("2. QPHAN / USDC:", getPoolId(QPHAN, USDC));
+console.log("3. ECHO / USDC:", getPoolId(ECHO, USDC));
+console.log("4. WETH / USDC:", getPoolId(WETH, USDC));
+console.log("5. ETH / USDC:", getPoolId(ETH, USDC));
